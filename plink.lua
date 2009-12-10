9791257aa430082a73fb9b471432984b37c0ef0a
Bruners <bruners@gmail.com>
2009-12-10 20:06:20 +0100

    Add the mining trigger, update toc for 3.3 and fix a small nil error with channel linking

--- plink.lua
+++ plink.lua
@@ -10,12 +10,20 @@ local profList = {
 	["!inscription"] = "Inscription",
 	["!lw"] = "Leatherworking",
 	["!leatherworking"] = "Leatherworking",
+	["!mining"] = "Mining",
 }
-
 -- Custom text to send when sending to a channel
 local customText = " no fee, tips are welcome"
+
+-- Text to send when someone tries to request a profession you don't have
+local whisperText = "I dont have "
+
+-- Text to print when you try to send a profession you don't have to a channel
+local channelLinkText = "You don't seem to have that profession"
+
 -- guildDelay/whisperDelay wait time before sending guild/whisper
 local guildDelay, whisperDelay = 3, 3
+
 -- Store time sent and who requested the link
 local spamTable = {}
 
@@ -37,7 +45,7 @@ function f:CHAT_MSG_WHISPER(event, msg, author, ...)
 					SendChatMessage(spell, "WHISPER", nil, author)
 					spamTable[author] = currentTime
 				else
-					SendChatMessage("I dont have " .. v, "WHISPER", nil, author)
+					SendChatMessage(whisperText .. v, "WHISPER", nil, author)
 				end
 				break
 			end
@@ -49,11 +57,11 @@ function f:CHAT_MSG_GUILD(event, msg, author, ...)
 	if(author == UnitName("player")) then return end
 
 	for k,v in pairs(profList) do
-		if msg:lower() == k or msg:sub(0,4) == k:sub(0,4) then
+		if msg:lower() == k or msg:sub(0,3) == k:sub(0,3) then
 			local currentTime = GetTime()
 			if not spamTable[author] or currentTime > (spamTable[author]+guildDelay) then
-				local spell = select(2,GetSpellLink(v))
-				if spell then
+				local spellCheck = select(2,GetSpellLink(v))
+				if spellCheck then
 					SendChatMessage(spell, "GUILD", nil)
 					spamTable[author] = currentTime
 				end
@@ -74,18 +82,23 @@ SlashCmdList['BLIB_PROF'] = function(arg1)
 	local prof
 	for link, spell in pairs(profList) do
 		if(arg1:lower() == link:sub(2)) then
-			prof = spell
-			break
+			local spellCheck = select(2,GetSpellLink(spell))
+			if spellCheck then
+				prof = spell
+				break
+			end
 		end
 	end
 
 	if prof then
 		if(type == "WHISPER") then
 			SendChatMessage(select(2,GetSpellLink(prof)), type, nil, ChatFrameEditBox:GetAttribute"tellTarget")
-		elseif ( type == "CHANNEL") then
+		elseif ( type == "CHANNEL")then
 			SendChatMessage(select(2,GetSpellLink(prof)) .. customText, type, nil, ChatFrameEditBox:GetAttribute"channelTarget")
 		else
 			SendChatMessage(select(2,GetSpellLink(prof)), type)
 		end
+	else
+		print("|cFF37FDFCb|rProflink: " .. channelLinkText)
 	end
 end
